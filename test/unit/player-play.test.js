582419fd891584bca3ef5c7bf8fdc05903ac9bde
Matt Luedke <mluedke.ds@gmail.com>
2017-11-22 14:01:19 -0500

    fix(middleware): allow setSource to be synchronous
    Previously, calls to middleware.setSource always set the source asynchronously.
    However, there are common use cases for mobile devices where you need to be able to set
    the source synchronously. On mobile browsers, auto-playing videos are stopped, and so are
    calls to the HTML5 Video element's play function unless the browser can synchronously
    associate that function call with a user-initiated browser event (such as a "click" event).
    Mobile browsers currently cannot associate play requests with a user-initiated browser
    event if that play request is invoked from an asynchronous callback (setTimeout, Promise,
    etc).
    
    For example, consider a page that has a video player and a list of videos with a "play"
    button next to each video. When the user clicks a "play" button next to one of the videos,
    the click event handler will call `player.src({ src: 'example' })` on the video.js player
    instance, and then call `player.play()` immediately after. This works fine on desktop
    browsers because the current implementation of the play function waits for the sources to
    be set before playing. On mobile browsers, however, playback is blocked because the
    browser cannot associate the user's click event with the call to `play()` because the
    source-setting process is asynchronous, which causes the play() request to be
    asynchronous.
    
    This fix allows the call to `player.src({ src: 'example' })` (and the resulting call to
    `middleware.setSource`) to be synchronous, given that there is no middleware that calls
    its `next` callback asynchronously. That may not always be the case, but this fix is
    focused on supporting the scenario described above in the case where there is no
    asynchronous middleware.
    
    Fixes #4765

--- test/unit/player-play.test.js
+++ test/unit/player-play.test.js
@@ -2,6 +2,7 @@
 import sinon from 'sinon';
 import {silencePromise} from '../../src/js/utils/promise';
 import TestHelpers from './test-helpers';
+import * as middleware from '../../src/js/tech/middleware.js';
 
 QUnit.module('Player#play', {
 
@@ -92,15 +93,36 @@ QUnit.test('tech ready + has source = play immediately!', function(assert) {
 });
 
 QUnit.test('tech ready + has source + changing source = wait for loadstart', function(assert) {
+  // This test requires that the source changing process be asynchronous. If source
+  // changing were synchronous, the calls to `play()` would immediately succeed because
+  // the player only waits for the `loadstart` event when `play()` is called while the
+  // player is in the process of changing sources.
+  //
+  // Source changing is synchronous, unless there is asynchronous middleware. So, we add
+  // asynchronous middleware here to make source changing asynchronous.
+  const asyncMiddleware = {
+    setSource(_src, next) {
+      setTimeout(() => {
+        next(null, _src);
+      }, 1);
+    }
+  };
+
+  middleware.use('video/foo', () => asyncMiddleware);
 
   // Mock the player having a source and in the process of changing its source.
-  this.player.src('xyz.mp4');
+  this.player.src({ src: 'xyz.mp4', type: 'video/foo' });
   this.clock.tick(100);
-  this.player.src('abc.mp4');
+  this.player.src({ src: 'abc.mp4', type: 'video/foo' });
+
   this.player.play();
   this.clock.tick(100);
+
   assert.strictEqual(this.techPlayCallCount, 0, 'tech_.play was not called because the source was changing');
 
   this.player.trigger('loadstart');
   assert.strictEqual(this.techPlayCallCount, 1, 'tech_.play was called');
+
+  // Remove the middleware we added just for this test
+  middleware.getMiddleware('video/foo').pop();
 });
